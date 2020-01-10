c7c64343bc96312a0533226f4b33901a159e4923
Bob Yoon <pejapie@gmail.com>
2020-01-10 13:38:42 +0900

    Updates

---
+++ precache-manifest.867615fd145bb888b5b5ea1432205bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07f8175fd397f932477665c7e3095bd",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "102a4bf4b0b29000c322",
+    "url": "/movie_app/static/css/main.8bd1f407.chunk.css"
+  },
+  {
+    "revision": "c529b2fc5b41c1dd1c21",
+    "url": "/movie_app/static/js/2.e8ee7b20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.e8ee7b20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "102a4bf4b0b29000c322",
+    "url": "/movie_app/static/js/main.553fb86b.chunk.js"
+  },
+  {
+    "revision": "815f4153bf9a6b7be15f",
+    "url": "/movie_app/static/js/runtime-main.e7bf67e9.js"
+  }
+]);
